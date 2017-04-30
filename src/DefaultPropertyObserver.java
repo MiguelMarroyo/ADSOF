5e66a332bb979a2baba1ebedade4f848ec90339a
Miguel Marroyo <miguelmarroyob@gmail.com>
2017-04-30 14:00:04 +0200

    Apartado 1

---
+++ src/DefaultPropertyObserver.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Implementa la interfaz PropertyObserver
+ * 
+ * @author Miguel Angel Marroyo Bouzada miguel.marroyo@estudiante.uam.es
+ *         Angelica L Jimenez Monar angelical.jimenez@estudiante.uam.es
+ *
+ */
+public class DefaultPropertyObserver implements PropertyObserver{
+
+	@Override
+	public void propertyChanged(ObservableProperty property, Object oldValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
